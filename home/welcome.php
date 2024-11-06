1083c9bb2e4c760970bfb0668f4d42d120b0baec
Jayson Abad <thesayangtis@localhost.localdomain>
2024-11-07 00:59:49 +0800

    fourth

---
+++ home/welcome.php
@@ -0,0 +1,17 @@
+<!Doctype HTML>
+<html>
+
+<head>
+  <title>
+    Welcome
+  </title>
+</head>
+
+<body>
+
+  Welcome <?php echo $_POST["name"]; ?><br>
+  Your email address is: <?php echo $_POST["email"]; ?>
+
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -12,5 +12,11 @@
       echo "My car is " . $color . "<br>";
     ?>
 
+    <form action="home/welcome.php" method="POST">
+      Name: <input type="text" name="name"><br>
+      E-mail: <input type="text" name="email"><br>
+      <input type="submit">
+    </form>
+
 </body>
 </html>
